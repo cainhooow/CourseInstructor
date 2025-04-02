7db6c94080bad965ec24f59173514e5ca1f441cf
cainhooo <augustocaio663@gmail.com>
2025-04-02 13:28:50 -0300

    refactor: update CourseRequest validation rules and enhance course creation route with user ID

--- src/app/versions/v1/@me/courses/route.ts
+++ src/app/versions/v1/@me/courses/route.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import BaseRouter, { Post, Route } from "@/app/utils/BaseRouter";
 import RoleMiddleware from "@/app/middleware/RoleMiddleware";
+import { Validatate } from "@/app/http/requests/Request";
+import CourseRequest from "@/app/http/requests/course/CourseRequest";
+import { UserDTO } from "@/app/dto/user/UserDTO";
 
 @Route("/courses")
 export default class CourseRouter extends BaseRouter {
@@ -11,6 +14,11 @@ export default class CourseRouter extends BaseRouter {
   @Post("/", [
     new RoleMiddleware(["CAN_POST_COURSE", "CAN_CREATE_PAID_COURSE"]),
   ])
+  @Validatate(CourseRequest, (req) => ({
+    appendFields: {
+      userId: (req.user as UserDTO).id,
+    },
+  }))
   async create(req: Request, res: Response) {
     res.json({ message: "ok" });
   }
